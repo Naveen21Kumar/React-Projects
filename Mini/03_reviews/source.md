6693a7388163ee7e847e2f5d490d88d573d33f57
Naveen Kumar <kumar20maverick@gmail.com>
2024-01-23 21:41:19 +0530

    Completed mini project 03. Reviews

---
+++ Mini/03_reviews/source.md
@@ -0,0 +1,5 @@
+## Project Source Information
+
+### Platform Name: Udemy
+### Instructor Name : John Smilga
+### Link:  [React 18 Tutorial and Projects Course (2023)](https://www.udemy.com/course/react-tutorial-and-projects-course/learn/lecture/39099276#overview)
---
+++ Mini/03_reviews/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import people from './data';
+import { FaQuoteRight, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  // prev Person
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = (currentIndex - 1 + people.length) % people.length;
+      return newIndex;
+    });
+  };
+
+  // next person
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = (currentIndex + 1) % people.length;
+      return newIndex;
+    });
+  };
+
+  // random person
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    const newIndex = randomNumber % people.length;
+    setIndex(newIndex);
+  };
+  return (
+    <main>
+      <article className='review'>
+        <div className='img-container'>
+          <img src={image} alt={name} className='person-img' />
+          <span className='quote-icon'>
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
+        <div className='btn-container'>
+          <button className='prev-btn'>
+            <FaChevronLeft onClick={prevPerson} />
+          </button>
+          <button className='prev-btn'>
+            <FaChevronRight onClick={nextPerson} />
+          </button>
+        </div>
+        <button className='btn btn-hipster' onClick={randomPerson}>
+          surprise me
+        </button>
+      </article>
+    </main>
+  );
+};
+export default App;
